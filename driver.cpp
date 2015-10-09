f3b645848c7c74a06fdf4f56cfb35a8d6c06e490
NancyGomez <nancyhgomez@student.hartnell.edu>
2015-10-09 10:57:50 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,1 +1,34 @@
+// Nancy Gomez
+// Nicholas Roasas
+// Noe Lomeli
+// Michael Sanchez
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+char menu();
+double climbing(int people);
+double scuba(int people);
+double skyDive(int people);
+double spelunk(int people);
+
+int main(){
+  
+  return 0;
+}
+char menu(){
+  
+}
+double climbing(int people){
+  
+}
+double scuba(int people){
+  
+}
+double skyDive(int people){
+  
+}
+double spelunk(int people){
+  
+}
